54f2594f7b95a6d7a8ddab32bace083ba0eaea62
jamalashrafi <jamalnabi07@gmail.com>
2021-07-15 12:46:32 +0530

    add: added basic navigation using route, history, match, withRouter .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import { Route } from 'react-router-dom';
 import HomePage from './pages/Homepage.components';
+import HatsPage from './pages/HatsPage';
 
 function App() {
   return (
     <div >
-      <HomePage />
+      <Route exact path="/" component={HomePage} />
+      <Route path='/hats' component={HatsPage} />
     </div>
   );
 }
