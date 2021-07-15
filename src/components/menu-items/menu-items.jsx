64e121718c58741f9b4687be80c29c7a8ab8cd70
jamalashrafi <jamalnabi07@gmail.com>
2021-07-15 11:10:49 +0530

    add: added hover effect to the menu-item.

--- src/components/menu-items/menu-items.jsx
+++ src/components/menu-items/menu-items.jsx
@@ -3,7 +3,8 @@ import './menu-items.scss';
 
 const menuItems = ({ title, imageUrl, size }) => {
     return (
-        <div className={`${size} menu-item`} style={{ backgroundImage: `url(${imageUrl})` }}>
+        <div className={`${size} menu-item`}>
+            <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }}></div>
             <div className='content'>
                 <h1 className='title'>{title.toUpperCase()}</h1>
                 <span className='sub-title'>Some text</span>
